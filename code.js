1695cf0af493ed96a826f7968c736d77bcae8326
CadeMaynard <143521268+CadeMaynard@users.noreply.github.com>
2023-09-01 08:40:03 -0600

    Sources Added to code.js

--- code.js
+++ code.js
@@ -6,3 +6,13 @@ function sum(a) {
     }
     return sum;
 }
+
+/* 
+Sources:
+https://learning.oreilly.com/library/view/javascript-pocket-reference/9781449335977/ch09.html#id776296
+https://github.com/jsverify/jsverify
+https://www.npmjs.com/package/jsverify?activeTab=readme
+http://www.java2s.com/ref/javascript/javascript-array-forall-func-context.html
+https://stackoverflow.com/questions/15313418/what-is-assert-in-javascript
+The book: Javascript & AJAX for Dummies
+*/
